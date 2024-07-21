8c0659701b948611a59b35815634df96ab898466
Adekola <aabdulqowiy@gmail.com>
2024-07-21 21:18:57 +0100

    Initial commit

---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+export const sidebarLinks = [
+  {
+    label: "Home",
+    route: "/",
+    imgURL: "icons/Home.svg",
+  },
+  {
+    label: "Upcoming",
+    route: "/upcoming",
+    imgURL: "icons/upcoming.svg",
+  },
+  {
+    label: "Previous",
+    route: "/previous",
+    imgURL: "icons/previous.svg",
+  },
+  {
+    label: "Recordings",
+    route: "/recordings",
+    imgURL: "icons/Video.svg",
+  },
+  {
+    label: "Personal Room",
+    route: "/personal-room",
+    imgURL: "icons/add-personal.svg",
+  },
+];
